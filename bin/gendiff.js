be4dea9f8ec60106d3c13a5dd79553b7cf5c3c4c
Владислав Кирилюк <vladik15041999@gmail.com>
2022-09-07 18:23:03 +0300

    Review 3 fixes

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,9 +7,7 @@ const program = new Command();
 program
   .name('gendiff')
   .version('1.0.0')
-  .description('Compares two configuration files and shows a difference in the selected format');
-
-program
+  .description('Compares two configuration files and shows a difference in the selected format')
   .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -9,24 +9,18 @@ const getValue = (value) => {
   return `${value}`;
 };
 
-const plain = (data, acc = '') => {
-  const result = data.map((obj) => {
-    switch (obj.type) {
-      case 'added':
-        return `Property '${acc}${obj.key}' was added with value: ${getValue(obj.val)}`;
-      case 'removed':
-        return `Property '${acc}${obj.key}' was removed`;
-      case 'changed':
-        return `Property '${acc}${obj.key}' was updated. From ${getValue(obj.val1)} to ${getValue(obj.val2)}`;
-      case 'unchanged':
-        return 'unchanged';
-      case 'object':
-        return plain(obj.children, `${acc}${obj.key}.`);
-      default:
-        throw new Error('Unexpected type');
-    }
-  });
-  return [...result.filter((string) => string !== 'unchanged')].join('\n');
-};
-
+const plain = (data, acc = '') => data.reduce((result, obj) => {
+  switch (obj.type) {
+    case 'added':
+      return [...result, `Property '${acc}${obj.key}' was added with value: ${getValue(obj.val)}`];
+    case 'removed':
+      return [...result, `Property '${acc}${obj.key}' was removed`];
+    case 'changed':
+      return [...result, `Property '${acc}${obj.key}' was updated. From ${getValue(obj.val1)} to ${getValue(obj.val2)}`];
+    case 'object':
+      return [...result, plain(obj.children, `${acc}${obj.key}.`)];
+    default:
+      return result;
+  }
+}, []).join('\n');
 export default plain;
